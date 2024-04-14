ba2596584626a05e0e89ab1c721cce412cbe3d93
Jonatas Gonçalves <jonatasgoncalves@protonmail.com>
2024-04-14 01:19:35 -0300

    Update MaxxedSUSE-KDE.sh

--- MaxxedSUSE-KDE.sh
+++ MaxxedSUSE-KDE.sh
@@ -54,10 +54,10 @@ CYAN=$ESC_SEQ"36;01m"
 
 # Clone the repository to MaxxedSUSE folder in the home directory
 sudo zypper --non-interactive in git
-git clone https://github.com/Jonatas-Goncalves/MaxxedSUSE "$HOME/MaxxedSUSE"
+su $USER -c "git clone https://github.com/Jonatas-Goncalves/MaxxedSUSE "$HOME/MaxxedSUSE"
 
 # Change directory to MaxxedSUSE
-cd ~/MaxxedSUSE
+su $USER -c "cd "$HOME/MaxxedSUSE""
 
 # Execute o arquivo inicial do seu script
-sudo -sE bash ./Start.sh
+su $USER -c "sudo -sE bash ./Start.sh"
